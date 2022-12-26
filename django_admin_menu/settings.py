1ca3069157e8008312e0387d107d54668aaf19a5
kaminyv <kaminyv@yandex.ru>
2022-12-26 15:01:43 +0600

    Added a web application to demonstrate the menu_app application.

--- django_admin_menu/settings.py
+++ django_admin_menu/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'menu_app',
+    'web_app',
 ]
 
 if DEBUG:
--- django_admin_menu/urls.py
+++ django_admin_menu/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from web_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
 
 if settings.DEBUG:
